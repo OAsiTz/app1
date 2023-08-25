3b6eb5de2643d16a0f3e79cec6255ebcba7effef
OAsiTz <110074137+OAsiTz@users.noreply.github.com>
2023-08-25 15:53:02 +0700

    26.8.23

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import Calendar from './class-component';
 import Banner from './banner'
 import { Calculator } from './calculator';
 import {EventData2} from './event-data'
+import RefsFunc from './refs-func';
+import RefsArray from './refs-array';
+import MessageBox from './state-func';
+
+
+
 
 export default function App() {
   return (
     <>
-    <EventData2/>
+     <MessageBox/> 
     </>
   )
    
