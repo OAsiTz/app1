446d6826ddabd146f2e556ac5004912c831938c4
OAsiTz <110074137+OAsiTz@users.noreply.github.com>
2023-09-08 15:49:10 +0700

    9.8.23

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Content,Footer,Header} from './func-components';
+//import {Content,Footer,Header} from './func-components';
 import Calendar from './class-component';
 import Banner from './banner'
 import { Calculator } from './calculator';
@@ -7,15 +7,19 @@ import {EventData2} from './event-data'
 import RefsFunc from './refs-func';
 import RefsArray from './refs-array';
 import MessageBox from './state-func';
-
-
+import { userContext } from './context';
+import Header2 from './context-header2';
+import Content2 from './context-content2';
 
 
 export default function App() {
+  let[user,setUser] = React.useState()
   return (
-    <>
-     <MessageBox/> 
-    </>
+    <userContext.Provider value={[user,setUser]}>
+    <Header2/>
+    <Content2/>
+    
+    </userContext.Provider>
   )
    
 }
